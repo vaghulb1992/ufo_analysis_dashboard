d9f724750b80544981e4a1103510a705c2200ea4
Vaghul Aditya Balaji <vaghulb1992@gmail.com>
2019-03-08 12:33:32 -0800

    Minor changes to the layout and content

--- ufo_analysis_dashboard.py
+++ ufo_analysis_dashboard.py
@@ -4,6 +4,7 @@
 from textblob import TextBlob
 import plotly.graph_objs as go
 import pandas as pd
+import emoji
 
 #############################################################
 # DATA CLEANUP
@@ -87,68 +88,10 @@ app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 app.title = "UFO Sightings Analysis"
 
 app.layout = html.Div([
-    html.Hr(),
+    html.H2(emoji.emojize("UFO Spotting Guide :alien:")),
 
     dcc.Tabs(id = "tabs", children = [
-        dcc.Tab(label = "Sentiment Analysis", children = [
-
-            dcc.Graph(id = "sentiment-plot"),
-
-            html.Section([
-                html.Div(["Year", dcc.RangeSlider(id = 'sentiment-year-chooser',
-                                                  min = 1960,
-                                                  max = df.datetime.dt.year.max(),
-                                                  value = [1976, 2014],
-                                                  marks = {str(year): str(year) for year in df.datetime.dt.year.unique()[::4]}
-                                                  )
-                          ], style={'width': '96.75%', 'float': 'right', 'display': 'inline-block', 'padding': 30, 'padding-bottom': '35px'}),
-
-                html.Div(["Select a country/province",
-                          dcc.Dropdown(id = 'location-dropdown',
-                                       options = sentiment_location_list,
-                                       value = 'ca',
-                                       clearable = False
-                                       )
-                          ], style={'width': '20%', 'display': 'inline-block', 'padding-left': '10px', 'padding-bottom': '10px'})
-                          ], style={'background-color':'#F9F9F9', 'border-radius': '15px'})
-        ], selected_style = {'borderTop': '1px solid #d6d6d6',
-                             'borderBottom': '1px solid #d6d6d6',
-                             'backgroundColor': '#119DFF',
-                             'color': 'white'}
-        ),
-
-        dcc.Tab(label = "Latitude-Longitude Info", children = [
-
-            dcc.Graph(id = "lat-long-plot"),
-
-            dcc.Interval(id='interval-component',
-                         interval = 1000 * 60 * 120,
-                         n_intervals = 0),
-
-            html.Section([
-                    html.Div(["Year",
-                              dcc.Slider(id = 'year-chooser',
-                                         min = 1960,
-                                         max = df.datetime.dt.year.max(),
-                                         value = 1960,
-                                         marks = {str(year): str(year) for year in df.datetime.dt.year.unique()[::4]}
-                                         )
-                              ], style={'width': '96.75%', 'float': 'right', 'display': 'inline-block', 'padding': 30, 'padding-bottom': '35px'}),
-
-
-                    html.Button('Reset', id='reset', style = {'background-color': '#E33F06', 'color': 'white', 'padding-bottom': '35px'}),
-
-                    html.Button('Play/Pause', id = 'play-pause', style = {'background-color': '#E3F106', 'color': 'black', 'padding-bottom': '35px'})
-
-                        ], style={'background-color':'#F9F9F9', 'border-radius': '15px'})
-                    ], selected_style = {'borderTop': '1px solid #d6d6d6',
-                                         'borderBottom': '1px solid #d6d6d6',
-                                         'backgroundColor': '#119DFF',
-                                         'color': 'white'}),
-
-        dcc.Tab(label = "Spatial and Temporal Info", children = [
-
-            html.B([html.H5('Spatial and Temporal Distributions of UFO sightings')]),
+        dcc.Tab(label = "Where and when can I see it?", children = [
 
             html.Div([
                     html.Div(id = "choropleth-div", children = [
@@ -181,7 +124,66 @@ app.layout = html.Div([
                         ], style={'background-color':'#F9F9F9', 'border-radius': '15px'})
                     ], selected_style = {'borderTop': '1px solid #d6d6d6',
                                          'borderBottom': '1px solid #d6d6d6',
-                                         'backgroundColor': '#119DFF',
+                                         'backgroundColor': '#001580',
+                                         'color': 'white'}),
+
+        dcc.Tab(label = "How do people feel about it?", children = [
+
+            dcc.Graph(id = "sentiment-plot"),
+
+            html.Section([
+                html.Div(["Year", dcc.RangeSlider(id = 'sentiment-year-chooser',
+                                                  min = 1960,
+                                                  max = df.datetime.dt.year.max(),
+                                                  value = [1976, 2014],
+                                                  marks = {str(year): str(year) for year in df.datetime.dt.year.unique()[::4]}
+                                                  )
+                          ], style={'width': '96.75%', 'float': 'right', 'display': 'inline-block', 'padding': 30, 'padding-bottom': '35px'}),
+
+                html.Div(["Select a country/province",
+                          dcc.Dropdown(id = 'location-dropdown',
+                                       options = sentiment_location_list,
+                                       value = 'ca',
+                                       clearable = False
+                                       )
+                          ], style={'width': '20%', 'display': 'inline-block', 'padding-left': '10px', 'padding-bottom': '10px'})
+                          ], style={'background-color':'#F9F9F9', 'border-radius': '15px'})
+        ], selected_style = {'borderTop': '1px solid #d6d6d6',
+                             'borderBottom': '1px solid #d6d6d6',
+                             'backgroundColor': '#001580',
+                             'color': 'white'}
+        ),
+
+        dcc.Tab(label = "How do I find out about it?", children = [
+
+            dcc.Graph(id = "lat-long-plot"),
+
+            dcc.Interval(id='interval-component',
+                         interval = 1000 * 60 * 120,
+                         n_intervals = 0),
+
+            html.Div(id = "communication_div",
+                     style = {'text-align': 'center', 'padding-top': '30px', 'font-size': '17px'}),
+
+            html.Section([
+                    html.Div(["Year",
+                              dcc.Slider(id = 'year-chooser',
+                                         min = 1960,
+                                         max = df.datetime.dt.year.max(),
+                                         value = 1960,
+                                         marks = {str(year): str(year) for year in df.datetime.dt.year.unique()[::4]}
+                                         )
+                              ], style={'width': '96.75%', 'float': 'right', 'display': 'inline-block', 'padding': 30, 'padding-bottom': '35px'}),
+
+
+                    html.Button('Reset', id='reset', style = {'background-color': '#E33F06', 'color': 'white', 'padding-bottom': '35px'}),
+
+                    html.Button('Play/Pause', id = 'play-pause', style = {'background-color': '#E3F106', 'color': 'black', 'padding-bottom': '35px'})
+
+                        ], style={'background-color':'#F9F9F9', 'border-radius': '15px'})
+                    ], selected_style = {'borderTop': '1px solid #d6d6d6',
+                                         'borderBottom': '1px solid #d6d6d6',
+                                         'backgroundColor': '#001580',
                                          'color': 'white'})
     ], style = {'height': '44px', 'borderBottom': '1px solid #d6d6d6', 'fontWeight': 'bold'})
 ], style={'width': '100%', 'display': 'inline-block'})
@@ -190,9 +192,9 @@ app.layout = html.Div([
 # Dash callbacks
 #############################
 
-##################
-# First tab
-##################
+###########################
+# Sentiment Analysis Tab
+###########################
 
 @app.callback(Output('sentiment-plot', 'figure'),
               [Input('sentiment-year-chooser', 'value'), Input('location-dropdown', 'value')])
@@ -227,9 +229,9 @@ def sentiment_line_callback(slider_value, location_name):
     traces = [positive_line, negative_line]
 
     if(start_year == end_year):
-        title = f"UFO comments sentiment analysis for {start_year}"
+        title = f"Sentiment analysis of comments made on UFO sightings for {start_year}"
     else:
-        title = f"UFO comments sentiment analysis between {start_year} - {end_year}"
+        title = f"Sentiment analysis of comments made on UFO sightings between {start_year} - {end_year}"
 
     if(location_name == "ca"):
         title = title + " in Canada"
@@ -245,9 +247,14 @@ def sentiment_line_callback(slider_value, location_name):
 
     return dict(data = traces, layout = layout)
 
-##################
-# Second tab
-##################
+###########################
+# Animation Tab
+###########################
+
+# we need this variable to be able to dynamically control the year that is part of the animation
+MIN_YEAR = 1960
+MAX_YEAR = 2014
+cur_year = MIN_YEAR
 
 @app.callback(Output('interval-component', 'interval'),
               [Input('play-pause', 'n_clicks')])
@@ -268,17 +275,41 @@ def animation_play_pause_color_callback(n_clicks):
 @app.callback(Output('interval-component', 'n_intervals'),
               [Input('reset', 'n_clicks')])
 def animation_reset_callback(n_clicks):
+    global cur_year
+    cur_year = MIN_YEAR
     return 0
 
 @app.callback(Output('year-chooser', 'value'),
               [Input('interval-component', 'n_intervals')])
 def interval_year_callback(n_intervals):
-    year_delta = n_intervals % 55
-    return (1960 + year_delta)
+    global cur_year
+
+    if(n_intervals == 0):
+        return MIN_YEAR
+
+    if(cur_year == MAX_YEAR):
+        return MAX_YEAR
+    else:
+        cur_year = cur_year + 1
+        return cur_year
+
+@app.callback(Output('communication_div', 'children'),
+              [Input('year-chooser', 'value')])
+def communication_text_callback(year_chosen):
+    year = int(year_chosen)
+    if(year <= 1990):
+        return f"Main mode of communication: Letters/Telephones"
+    elif(year > 1990 and year <= 2007):
+        return f"Main mode of communication: Emails/Mobile Phones"
+    else:
+        return f"Main mode of communication: Twitter/Facebook/Smart Phones"
 
 @app.callback(Output('lat-long-plot', 'figure'),
               [Input('year-chooser', 'value')])
 def dot_map_callback(year_chosen):
+    global cur_year
+    cur_year = int(year_chosen)
+
     # make figure
     figure = {'data': [], 'layout': {}}
 
@@ -316,9 +347,9 @@ def dot_map_callback(year_chosen):
 
     return figure
 
-##################
-# Third tab
-##################
+###########################
+# Spatial-Temporal Tab
+###########################
 
 @app.callback(Output('choropleth-div', 'style'),
               [Input('province-dropdown', 'value')])
@@ -382,8 +413,10 @@ def barplot_callback(slider_value, province_name):
     bar_trace = go.Bar(x = month_data.index,
                        y = month_data,
                        name = "UFO sightings by month",
-                       marker = dict(color = "#F4D03F"))
-
+                       marker = dict(color = ["#5DADE2" if month in (12, 1, 2) else
+                                              "#1ABC9C" if month in (3, 4, 5) else
+                                              "#F4D03F" if month in (6, 7, 8) else
+                                              "#9C2706" for month in month_data.index]))
     traces = [bar_trace]
     if(start_year == end_year):
         title = f"UFO sightings by month in {start_year}"
@@ -409,7 +442,9 @@ def choropleth_callback(slider_value):
     end_year = int(slider_value[1])
     subset_df = df[(df.datetime.dt.year >= start_year) & (df.datetime.dt.year <= end_year)]
 
-    scl = [[0.0, 'rgb(165,0,38)'], [0.1111111111111111, 'rgb(215,48,39)'], [0.2222222222222222, 'rgb(244,109,67)'], [0.3333333333333333, 'rgb(253,174,97)'], [0.4444444444444444, 'rgb(254,224,144)'], [0.5555555555555556, 'rgb(224,243,248)'], [0.6666666666666666, 'rgb(171,217,233)'], [0.7777777777777778, 'rgb(116,173,209)'], [0.8888888888888888, 'rgb(69,117,180)'], [1.0, 'rgb(49,54,149)']]
+    scl = [[0.0, '#E7E625'], [0.1, '#E7E625'], [0.2, '#E79A25'], [0.3, '#E79A25'],
+           [0.4, '#B80707'], [0.5, '#B80707'], [0.6, '#B80707'],
+           [0.7, '#B80707'], [0.8, '#B80707'], [0.9, '#B80707'], [1.0, '#B80707']]
 
     data = [dict(type='choropleth',
                  colorscale = scl,
